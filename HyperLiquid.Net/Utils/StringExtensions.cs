c031bef3ce96f90d208dd91a9c32ab4d1346f2ee
Jkorf <jankorf91@gmail.com>
2025-04-10 10:46:18 +0200

    Update StringExtensions.cs

--- HyperLiquid.Net/Utils/StringExtensions.cs
+++ HyperLiquid.Net/Utils/StringExtensions.cs
@@ -6,58 +6,58 @@ namespace HyperLiquid.Net.Utils
     {
         public static byte[] HexToByteArray(this string value)
         {
+            byte[] bytes;
+            if (string.IsNullOrEmpty(value))
             {
-                byte[] bytes;
-                if (string.IsNullOrEmpty(value))
-                {
-                    bytes = Array.Empty<byte>();
-                }
-                else
-                {
-                    var string_length = value.Length;
-                    var character_index = value.StartsWith("0x", StringComparison.Ordinal) ? 2 : 0;
-                    // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
-                    var number_of_characters = string_length - character_index;
-
-                    var add_leading_zero = false;
-                    if (0 != number_of_characters % 2)
-                    {
-                        add_leading_zero = true;
-
-                        number_of_characters += 1; // Leading '0' has been striped from the string presentation.
-                    }
-
-                    bytes = new byte[number_of_characters / 2]; // Initialize our byte array to hold the converted string.
-
-                    var write_index = 0;
-                    if (add_leading_zero)
-                    {
-                        bytes[write_index++] = FromCharacterToByte(value[character_index], character_index);
-                        character_index += 1;
-                    }
-
-                    for (var read_index = character_index; read_index < value.Length; read_index += 2)
-                    {
-                        var upper = FromCharacterToByte(value[read_index], read_index, 4);
-                        var lower = FromCharacterToByte(value[read_index + 1], read_index + 1);
-
-                        bytes[write_index++] = (byte)(upper | lower);
-                    }
-                }
-
-                return bytes;
+                bytes = Array.Empty<byte>();
             }
+            else
+            {
+                var stringLength = value.Length;
+                var characterIndex = value.StartsWith("0x", StringComparison.Ordinal) ? 2 : 0;
+                // Does the string define leading HEX indicator '0x'. Adjust starting index accordingly.               
+                var numberOfCharacters = stringLength - characterIndex;
+
+                var addLeadingZero = false;
+                if (0 != numberOfCharacters % 2)
+                {
+                    addLeadingZero = true;
+                    numberOfCharacters += 1; // Leading '0' has been striped from the string presentation.
+                }
+
+                bytes = new byte[numberOfCharacters / 2]; // Initialize our byte array to hold the converted string.
+
+                var writeIndex = 0;
+                if (addLeadingZero)
+                {
+                    bytes[writeIndex++] = FromCharacterToByte(value[characterIndex], characterIndex);
+                    characterIndex += 1;
+                }
+
+                for (var read_index = characterIndex; read_index < value.Length; read_index += 2)
+                {
+                    var upper = FromCharacterToByte(value[read_index], read_index, 4);
+                    var lower = FromCharacterToByte(value[read_index + 1], read_index + 1);
+
+                    bytes[writeIndex++] = (byte)(upper | lower);
+                }
+            }
+
+            return bytes;
         }
+
         private static byte FromCharacterToByte(char character, int index, int shift = 0)
         {
             var value = (byte)character;
             if (0x40 < value && 0x47 > value || 0x60 < value && 0x67 > value)
             {
                 if (0x40 == (0x40 & value))
+                {
                     if (0x20 == (0x20 & value))
                         value = (byte)((value + 0xA - 0x61) << shift);
                     else
                         value = (byte)((value + 0xA - 0x41) << shift);
+                }
             }
             else if (0x29 < value && 0x40 > value)
             {
