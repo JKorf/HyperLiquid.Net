aed3075110d53f6bf497ae1c26ce62f7077d0044
Jkorf <jankorf91@gmail.com>
2026-02-16 10:13:37 +0100

    Added staking endpoints

---
+++ HyperLiquid.Net/Objects/Models/HyperLiquidStakingDelegation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace HyperLiquid.Net.Objects.Models
+{
+    /// <summary>
+    /// Staking delegation info
+    /// </summary>
+    public record HyperLiquidStakingDelegation
+    {
+        /// <summary>
+        /// Validator address
+        /// </summary>
+        [JsonPropertyName("validator")]
+        public string Validator { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Amount delegated
+        /// </summary>
+        [JsonPropertyName("amount")]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Locked until timestamp
+        /// </summary>
+        [JsonPropertyName("lockedUntilTimestamp")]
+        public DateTime? LockedUntil { get; set; }
+    }
+}
---
+++ HyperLiquid.Net/Objects/Models/HyperLiquidStakingSummary.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HyperLiquid.Net.Objects.Models
+{
+    /// <summary>
+    /// Staking summary
+    /// </summary>
+    public record HyperLiquidStakingSummary
+    {
+        /// <summary>
+        /// Total amount delegated to validators
+        /// </summary>
+        [JsonPropertyName("delegated")]
+        public decimal Delegated { get; set; }
+
+        /// <summary>
+        /// Amount in staking balance but not delegated
+        /// </summary>
+        [JsonPropertyName("undelegated")]
+        public decimal Undelegated { get; set; }
+
+        /// <summary>
+        /// Total amount pending withdrawal (in 7-day queue)
+        /// </summary>
+        [JsonPropertyName("totalPendingWithdrawal")]
+        public decimal TotalPendingWithdrawal { get; set; }
+
+        /// <summary>
+        /// Number of pending withdrawals
+        /// </summary>
+        [JsonPropertyName("nPendingWithdrawals")]
+        public int PendingWithdrawals { get; set; }
+    }
+}
