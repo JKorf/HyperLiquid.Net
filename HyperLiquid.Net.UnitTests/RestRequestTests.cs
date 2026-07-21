d5fd5ad156aff652b7ec33d08a87651c961fdbee
Jkorf <jankorf91@gmail.com>
2026-07-21 11:31:30 +0200

    Fixed test

--- HyperLiquid.Net.UnitTests/RestRequestTests.cs
+++ HyperLiquid.Net.UnitTests/RestRequestTests.cs
@@ -51,7 +51,7 @@ namespace HyperLiquid.Net.UnitTests
             await tester.ValidateAsync(client => client.SpotApi.Account.GetRateLimitsAsync(), "GetRateLimits");
             await tester.ValidateAsync(client => client.SpotApi.Account.GetApprovedBuilderFeeAsync(), "GetApprovedBuilderFee");
             await tester.ValidateAsync(client => client.SpotApi.Account.GetSubAccountsAsync(), "GetSubAccounts");
-            await tester.ValidateAsync(client => client.SpotApi.Account.GetSubAccounts2Async(), "GetSubAccounts2");
+            await tester.ValidateAsync(client => client.SpotApi.Account.GetSubAccounts2Async(), "GetSubAccounts2", ignoreProperties: ["dexToClearinghouseState", "tokenToPortfolioBorrowRatio", "tokenToPortfolioSupplyRatio", "tokenToAvailableAfterMaintenance"]);
             await tester.ValidateAsync(client => client.SpotApi.Account.GetExtraAgentsAsync(), "GetExtraAgents");
         }
 
